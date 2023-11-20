4c54205591d026acbcd57bf8a58d4886c0b6e993
othinAbbey <othincore@gmail.com>
2023-11-20 20:59:03 +0300

    cart

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Link from 'next/link';
 
 const Header = ({ user, onLogout }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -13,9 +14,11 @@ const Header = ({ user, onLogout }) => {
         <button className="text-white font-bold">Home</button>
       </div>
       <div className="flex items-center space-x-4">
+        <Link href="/products">
         <button className="text-white">
           Cart
         </button>
+        </Link>
         {user ? (
           <div className="relative">
             <button onClick={toggleDropdown} className="text-white">
@@ -30,11 +33,16 @@ const Header = ({ user, onLogout }) => {
             )}
           </div>
         ) : (
-          <div className="flex space-x-4">
-            <button className="text-white">Create Account</button>
-            <button className="text-white" >Login</button>
-            <button className="text-white">Sign Out</button>
-          </div>
+          <Link href="/login">
+            <button className="text-white">Login</button>
+          </Link>
+
+          // <div className="flex space-x-4">
+          //   <button className="text-white">Create Account</button>
+            
+          //   <button className="text-white" href="./login">Login</button>
+          //   <button className="text-white">Sign Out</button>
+          // </div>
         )}
       </div>
     </header>
