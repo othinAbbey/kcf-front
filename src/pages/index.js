7ac644f189720a701264db4b098c551f702ea232
othinAbbey <othincore@gmail.com>
2023-12-04 21:48:57 +0300

    j

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,91 @@
+// import React from 'react';
+// import Image from 'next/image';
+// import { useRouter } from 'next/router';
+// import Header from './header';
+
+// const Home = () => {
+//   const router = useRouter();
+
+//   return (
+//     <main className="min-h-screen bg-gray-100 dark:bg-black p-8" style={{
+//       // backgroundImage: "url('/background.jpg')",
+//       backgroundSize: 'cover',
+//       backgroundPosition: 'center',
+//     }}>
+//       {router.pathname !== '/' && <Header />}
+//       <section className="text-center">
+//         <h1 className="text-4xl font-bold mb-4 text-blue-700">
+//           Welcome to KCF-Online Shopping
+//         </h1>
+//         <p className="text-lg text-gray-700 dark:text-gray-300 mb-8">
+//           Discover a wide range of products at your fingertips.
+//         </p>
+//       </section>
+
+//       {/* Features Section */}
+//       <section className="mt-12 grid gap-8 lg:grid-cols-3">
+//         {/* Feature 1 */}
+//         <div className="text-center">
+//           <h2 className="text-xl font-semibold mb-2 text-green-700">
+//             Create an Account
+//           </h2>
+//           <p className="text-gray-700 dark:text-gray-300">
+//             Easily create an account to access exclusive features and follow up on your shoppings.
+//           </p>
+//         </div>
+
+//         {/* Feature 2 */}
+//         <div className="text-center">
+//           <h2 className="text-xl font-semibold mb-2 text-purple-700">
+//             Explore Products
+//           </h2>
+//           <p className="text-gray-700 dark:text-gray-300">
+//             Browse through a diverse range of products with detailed information.
+//           </p>
+//         </div>
+
+//         {/* Feature 3 */}
+//         <div className="text-center">
+//           <h2 className="text-xl font-semibold mb-2 text-orange-700">
+//             Easy Shopping Cart
+//           </h2>
+//           <p className="text-gray-700 dark:text-gray-300">
+//             Effortlessly manage your shopping cart and orders.
+//           </p>
+//         </div>
+//       </section>
+
+//       {/* Call to Action */}
+//       <section className="mt-12 text-center">
+//         <p className="text-gray-600 dark:text-gray-400 mb-4">
+//           Ready to get started?
+//         </p>
+//         <a
+//           href="/signup"
+//           className="bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-600 transition inline-block"
+//         >
+//           Sign Up Now
+//         </a>
+
+//         <br />
+
+//         <p className="mt-6">
+//           Already have an account?{' '}
+//           <a
+//             href="/login"
+//             className="text-blue-500 hover:underline transition"
+//           >
+//             Log In
+//           </a>
+//         </p>
+//       </section>
+//     </main>
+//   );
+// };
+
+// export default Home;
+
+
 import React from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -7,17 +95,13 @@ const Home = () => {
   const router = useRouter();
 
   return (
-    <main className="min-h-screen bg-gray-100 dark:bg-gray-800 p-8" style={{
-      backgroundImage: "url('/background.jpg')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}>
+    <main className="min-h-screen bg-black p-8 text-white">
       {router.pathname !== '/' && <Header />}
       <section className="text-center">
-        <h1 className="text-4xl font-bold mb-4 text-blue-700">
+        <h1 className="text-4xl font-bold mb-4 text-blue-500">
           Welcome to KCF-Online Shopping
         </h1>
-        <p className="text-lg text-gray-700 dark:text-gray-300 mb-8">
+        <p className="text-lg text-gray-300 mb-8">
           Discover a wide range of products at your fingertips.
         </p>
       </section>
@@ -26,30 +110,30 @@ const Home = () => {
       <section className="mt-12 grid gap-8 lg:grid-cols-3">
         {/* Feature 1 */}
         <div className="text-center">
-          <h2 className="text-xl font-semibold mb-2 text-green-700">
+          <h2 className="text-xl font-semibold mb-2 text-green-500">
             Create an Account
           </h2>
-          <p className="text-gray-700 dark:text-gray-300">
+          <p className="text-gray-300">
             Easily create an account to access exclusive features and follow up on your shoppings.
           </p>
         </div>
 
         {/* Feature 2 */}
         <div className="text-center">
-          <h2 className="text-xl font-semibold mb-2 text-purple-700">
+          <h2 className="text-xl font-semibold mb-2 text-purple-500">
             Explore Products
           </h2>
-          <p className="text-gray-700 dark:text-gray-300">
+          <p className="text-gray-300">
             Browse through a diverse range of products with detailed information.
           </p>
         </div>
 
         {/* Feature 3 */}
         <div className="text-center">
-          <h2 className="text-xl font-semibold mb-2 text-orange-700">
+          <h2 className="text-xl font-semibold mb-2 text-orange-500">
             Easy Shopping Cart
           </h2>
-          <p className="text-gray-700 dark:text-gray-300">
+          <p className="text-gray-300">
             Effortlessly manage your shopping cart and orders.
           </p>
         </div>
@@ -57,7 +141,7 @@ const Home = () => {
 
       {/* Call to Action */}
       <section className="mt-12 text-center">
-        <p className="text-gray-600 dark:text-gray-400 mb-4">
+        <p className="text-gray-400 mb-4">
           Ready to get started?
         </p>
         <a
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,109 @@
 
 
+// import React, { useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { useAuth } from "../authContext" 
+
+// const Login = () => {
+//   const router = useRouter();
+//   const { login } = useAuth();
+
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     password: '',
+//   });
+
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       // const response = await fetch('https://kcf-onlineshop.onrender.com/auth/login', {
+//       const response = await fetch('http://localhost:8000/auth/login', {
+        
+        
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           email: formData.email,
+//           password: formData.password,
+//         }),
+//       });
+
+//       const responseBody = await response.text();
+
+//       if (!response.ok) {
+//         alert(`Failed to log in due to : ${responseBody}`);
+//       } else {
+//         alert('Successfully logged in');
+//         login(/* Pass user data here if needed */);
+//         router.push('/products');
+//       }
+
+//       setFormData({
+//         email: '',
+//         password: '',
+//       });
+//     } catch (error) {
+//       console.error('Error logging in:', error.message);
+//     }
+//   };
+
+//   return (
+   
+//     <div className="min-h-screen flex items-center justify-center">
+//       <div className="bg-white dark:bg-gray-800 p-8 rounded-md shadow-md w-full max-w-md">
+//         <h2 className="text-3xl font-bold mb-8 text-center">Login</h2>
+//         <form onSubmit={handleSubmit} className="space-y-4">
+//           <div className="mb-4">
+//             <label htmlFor="email" className="block text-sm font-medium text-black dark:text-white">
+//               Email:
+//             </label>
+//             <input
+//               type="email"
+//               id="email"
+//               name="email"
+//               value={formData.email}
+//               onChange={handleChange}
+//               className="mt-1 p-2 w-full border rounded-md"
+//             />
+//           </div>
+//           <div className="mb-4">
+//             <label htmlFor="password" className="block text-sm font-medium text-black dark:text-white">
+//               Password:
+//             </label>
+//             <input
+//               type="password"
+//               id="password"
+//               name="password"
+//               value={formData.password}
+//               onChange={handleChange}
+//               className="mt-1 p-2 w-full border rounded-md"
+//             />
+//           </div>
+//           <button
+//             type="submit"
+//             className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 w-full transition"
+//             onClick={handleSubmit}
+//           >
+//             Login
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { useAuth } from "../authContext" 
+import { useAuth } from "../authContext";
 
 const Login = () => {
   const router = useRouter();
@@ -21,10 +122,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      // const response = await fetch('https://kcf-onlineshop.onrender.com/auth/login', {
       const response = await fetch('http://localhost:8000/auth/login', {
-        
-        
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -38,7 +136,7 @@ const Login = () => {
       const responseBody = await response.text();
 
       if (!response.ok) {
-        alert(`Failed to log in due to : ${responseBody}`);
+        alert(`Failed to log in due to: ${responseBody}`);
       } else {
         alert('Successfully logged in');
         login(/* Pass user data here if needed */);
@@ -55,8 +153,7 @@ const Login = () => {
   };
 
   return (
-   
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
       <div className="bg-white dark:bg-gray-800 p-8 rounded-md shadow-md w-full max-w-md">
         <h2 className="text-3xl font-bold mb-8 text-center">Login</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
@@ -89,7 +186,6 @@ const Login = () => {
           <button
             type="submit"
             className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 w-full transition"
-            onClick={handleSubmit}
           >
             Login
           </button>
@@ -100,4 +196,3 @@ const Login = () => {
 };
 
 export default Login;
-
